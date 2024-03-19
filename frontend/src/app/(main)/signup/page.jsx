52a516dd6ab198dd38c91d0183a3a3a338ae2866
anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-03-19 07:59:50 +0530

    chnge 19/03/24

--- frontend/src/app/(main)/signup/page.jsx
+++ frontend/src/app/(main)/signup/page.jsx
@@ -1,204 +1,215 @@
-'use client';
-import React from 'react';
-import { useFormik } from 'formik';
-import Link from 'next/link';
-import * as Yup from 'yup';
+import React from 'react'
 
-const sighupValidationSchema = Yup.object().shape(
-  {
-    
-    name : Yup.string().min(3,'3 Character Kam Nahi').max(20,'20 Character Se Jyada Nahi').required('Apna Name Likhna Bhool Gaye !'),
-    lname : Yup.string().required("Last Name is required"),
-    cnumber :  Yup.number().min(10, "Phone number must be at least 10 digits").required( "Contact Number is Required"),
-    email : Yup.string().email('Invalid Email').required( 'Apna Email Banao'),
-    password : Yup.string().min(6,'6 Character Kam Nahi !').max(12,'12 Character Se Jyada Nahi').required('Phle Password Set Karo !')
-    .matches(/[A-Z]/,'Password mudt contain uppercase letter')
-    .matches (/[a-z]/, 'Password must contain lowercase letter')
-    .matches(/[0-9]/, 'Password must contain number'),
-
-    cpassword : Yup.string().required('Phle Password Set Karo !')
-    .oneOf([Yup.ref('password'),null],'Password must cotain matches')
-
-  }
-);
-const Signup = () => {
-
-  const signupForm = useFormik (
-    {
-      initialValues : {
-        
-        name : '',
-        lname:'',
-        cnumber:'',
-        email :'',
-        password : '',
-        cpassword: ''
-      },
-      onSubmit : (values) => {
-        console.log(values);
-        //send data to backend
-      },
-      validationSchema:sighupValidationSchema
-    }
-  )
-
-
+const signup = () => {
   return (
-    <section className="vh-100 bg-primary-subtle">
-      <div className="container py-5 h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col">
-            <div className="card shadow my-4">
-              <div className="row g-0">
-                <div className="col-xl-6 d-none d-xl-block">
-                  <div style={{
-                    backgroundImage: `url('https://assets.materialup.com/uploads/7563d4bc-0ed9-4202-a86c-ac8dc46e73ef/preview.jpg')`,
-                    height: '100%',
-                    backgroundPosition: 'center',
-                    backgroundSize: 'contain',
-                    backgroundRepeat: 'no-repeat'
-                  }}>
-
-                  </div>
-                  {/* <img
-                    src="https://assets.materialup.com/uploads/7563d4bc-0ed9-4202-a86c-ac8dc46e73ef/preview.jpg"
-                    alt="Sample"
-                    className="img-fluid"
-                  /> */}
-                </div>
-                <div className="col-xl-6">
-
-                  <div className="card-body p-md-5">
-                    <h3 className="mb-5 text-primary fw-bold  fs-1">
-                      Registration Form
-                    </h3>
-                    <form onSubmit={signupForm.handleSubmit}>
-
-                      
-                      <div className="mb-3">
-                        <div className='row'>
-                        <div className='col-6'>
-                        <label for="" className="form-label">Name</label>
-                        <input
-                          type="text"
-                          id="name"
-                          onChange={ signupForm.handleChange }
-                          value={signupForm.values.name}
-                          className="form-control"
-                          placeholder="Apna Naam Likhe !"
-                        />{ signupForm.touched.name &&
-                          <small className="text-danger">{signupForm.errors.name}</small>
-                          }
-                        </div>
-                        <div className='col-6'>
-                        <label for="" className="form-label">Last Name</label>
-                        <input
-                          type="text"
-                          id="lname"
-                          onChange={ signupForm.handleChange }
-                          value={signupForm.values.lname}
-                          className="form-control"
-                          placeholder="Apna Last Naam Likhe !"
-                        />{ signupForm.touched.lname &&
-                          <small className="text-danger">{signupForm.errors.lname}</small>
-                          }
-                        </div>
-                        </div>
-                     </div>
-                      <div className="mb-3">
-                        <label for="" className="form-label">Contact Number</label>
-                        <input
-                          type="number"
-                          id="cnumber"
-                          onChange={ signupForm.handleChange }
-                          value={ signupForm.values.cnumber }
-                          className="form-control"
-                          placeholder="Apna Password Banao !"
-                        />{
-                          signupForm.touched.cnumber &&
-                        <small className="text-danger">{signupForm.errors.cnumber}</small>
-                        }
-                      </div>
-                      <div className="mb-3">
-                        <label for="" className="form-label">Email Address</label>
-                        <input
-                          type="text"
-                          id="email"
-                          onChange={ signupForm.handleChange }
-                          value={signupForm.values.email}
-                          className="form-control"
-                          placeholder="Apna New Email Likhe !"
-                        />
-                        {
-                          signupForm.touched.email &&
-                        <small className="text-danger">{signupForm.errors.email}</small>
-                        }
-                      </div>
-                      <div className="mb-3">
-                        <label for="" className="form-label">Password</label>
-                        <input
-                          type="password"
-                          id="password"
-                          onChange={ signupForm.handleChange }
-                          value={ signupForm.values.password }
-                          className="form-control"
-                          placeholder="Apna Password Banao !"
-                        />{
-                          signupForm.touched.password &&
-                        <small className="text-danger">{signupForm.errors.password}</small>
-                        }
-                      </div>
-                      <div className="mb-3">
-                        <label for="" className="form-label">Confirm Password</label>
-                        <input
-                          type="password"
-                          id="cpassword"
-                          onChange={signupForm.handleChange}
-                          value={signupForm.values.cpassword}
-                          className="form-control"
-                          placeholder="Dubara Password Likhe !"
-                        />{
-                          signupForm.touched.cpassword &&
-                        <small className="text-danger">{signupForm.errors.cpassword}</small>
-                        }
-                      </div>
-                      <div className="form-check mb-4">
-                        <input
-                          className="form-check-input me-2"
-                          type="checkbox"
-                          defaultValue=""
-                          id="form2Example33"
-                          defaultChecked=""
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="form2Example33"
-                        >
-                          I Agree to Terms & Conditions
-                        </label>
-                      </div>
-                      <div className="d-flex justify-content-end pt-3">
-                        <button type="button" className="btn btn-light">
-                          Reset all
-                        </button>
-                        <button type="submit" className="btn btn-primary ms-2">
-                          Submit form
-                        </button>
-                      </div>
-                    </form>
-
-                    <p>Already Registered? <Link href='/login'>Login Here</Link></p>
-
-                  </div>
+    <div>
+      <main className="w-full max-w-md mx-auto p-6">
+  <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <div className="p-4 sm:p-7">
+      <div className="text-center">
+        <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
+          Sign up
+        </h1>
+        <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+          Already have an account?
+          <a
+            className="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            href="../examples/html/signin.html"
+          >
+            Sign in here
+          </a>
+        </p>
+      </div>
+      <div className="mt-5">
+        <button
+          type="button"
+          className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+        >
+          <svg
+            className="w-4 h-auto"
+            width={46}
+            height={47}
+            viewBox="0 0 46 47"
+            fill="none"
+          >
+            <path
+              d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
+              fill="#4285F4"
+            />
+            <path
+              d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
+              fill="#34A853"
+            />
+            <path
+              d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
+              fill="#FBBC05"
+            />
+            <path
+              d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
+              fill="#EB4335"
+            />
+          </svg>
+          Sign up with Google
+        </button>
+        <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">
+          Or
+        </div>
+        {/* Form */}
+        <form>
+          <div className="grid gap-y-4">
+            {/* Form Group */}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm mb-2 dark:text-white"
+              >
+                Email address
+              </label>
+              <div className="relative">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                  required=""
+                  aria-describedby="email-error"
+                />
+                <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                  <svg
+                    className="size-5 text-red-500"
+                    width={16}
+                    height={16}
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                    aria-hidden="true"
+                  >
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                  </svg>
                 </div>
               </div>
+              <p className="hidden text-xs text-red-600 mt-2" id="email-error">
+                Please include a valid email address so we can get back to you
+              </p>
             </div>
+            {/* End Form Group */}
+            {/* Form Group */}
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm mb-2 dark:text-white"
+              >
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                  required=""
+                  aria-describedby="password-error"
+                />
+                <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                  <svg
+                    className="size-5 text-red-500"
+                    width={16}
+                    height={16}
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                    aria-hidden="true"
+                  >
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                  </svg>
+                </div>
+              </div>
+              <p
+                className="hidden text-xs text-red-600 mt-2"
+                id="password-error"
+              >
+                8+ characters required
+              </p>
+            </div>
+            {/* End Form Group */}
+            {/* Form Group */}
+            <div>
+              <label
+                htmlFor="confirm-password"
+                className="block text-sm mb-2 dark:text-white"
+              >
+                Confirm Password
+              </label>
+              <div className="relative">
+                <input
+                  type="password"
+                  id="confirm-password"
+                  name="confirm-password"
+                  className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                  required=""
+                  aria-describedby="confirm-password-error"
+                />
+                <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                  <svg
+                    className="size-5 text-red-500"
+                    width={16}
+                    height={16}
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                    aria-hidden="true"
+                  >
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                  </svg>
+                </div>
+              </div>
+              <p
+                className="hidden text-xs text-red-600 mt-2"
+                id="confirm-password-error"
+              >
+                Password does not match the password
+              </p>
+            </div>
+            {/* End Form Group */}
+            {/* Checkbox */}
+            <div className="flex items-center">
+              <div className="flex">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                />
+              </div>
+              <div className="ms-3">
+                <label
+                  htmlFor="remember-me"
+                  className="text-sm dark:text-white"
+                >
+                  I accept the{" "}
+                  <a
+                    className="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    href="#"
+                  >
+                    Terms and Conditions
+                  </a>
+                </label>
+              </div>
+            </div>
+            {/* End Checkbox */}
+            <button
+              type="submit"
+              className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+            >
+              Sign up
+            </button>
           </div>
-        </div>
+        </form>
+        {/* End Form */}
       </div>
-    </section>
+    </div>
+  </div>
+</main>
 
+    </div>
   )
 }
 
-export default Signup;
+export default signup;
