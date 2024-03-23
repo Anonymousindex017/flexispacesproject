a8de9fd1bd93eaba0188201ca903af547cdbcc36
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-03-23 07:35:13 +0530

    enquire update

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -14,27 +14,15 @@ const Home = () => {
           We build a high - energy workspace
         </h1>
         <p className="mt-3 text-lg text-gray-800 dark:text-gray-400">
-          At  Flexispaces, we believe that the best way to get things done is in a place where ideas can fly and creativity knows no bounds.
-          Flexibilities are key to our
+          Flexibilities are key to our  approach. Our team of experts will design and customize your space according to your needs, so
+          Flexispaces is an innovative.
         </p>
         <div className="mt-5 lg:mt-8 flex flex-col items-center gap-2 sm:flex-row sm:gap-3">
-          <div className="w-full sm:w-auto">
-            <label htmlFor="hero-input" className="sr-only">
-              Search
-            </label>
-            <input
-              type="text"
-              id="hero-input"
-              name="hero-input"
-              className="py-3 px-4 block w-full xl:min-w-72 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-              placeholder="Enter work email"
-            />
-          </div>
           <a
             className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
             href="#"
           >
-            Request demo
+            Enquire Now
           </a>
         </div>
         {/* Brands */}
