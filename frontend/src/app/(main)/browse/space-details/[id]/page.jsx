a66c516f2d097d8a727c6f7edd92258cc336747e
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-04-03 20:31:08 +0530

    feature

--- frontend/src/app/(main)/browse/space-details/[id]/page.jsx
+++ frontend/src/app/(main)/browse/space-details/[id]/page.jsx
@@ -1,122 +1,336 @@
+import { Chicle } from 'next/font/google';
 import React from 'react'
 
 const spaceDetails = () => {
-  return (
-    <div>
-        <>
-  {/* Features */}
-  <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-    {/* Grid */}
-    <div className="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
-      <div>
-        <img
-          className="rounded-xl"
-          src="https://media.istockphoto.com/id/1410270664/photo/modern-style-office-with-exposed-concrete-floor-and-a-lot-of-plants.jpg?s=612x612&w=0&k=20&c=lBivR3vIWH4dnb6MUNkQtQsIisaUEnzl2f6Ozyr-Jis="
-          alt="Image Description"
-        />
-      </div>
-      {/* End Col */}
-      <div className="mt-5 sm:mt-10 lg:mt-0">
-        <div className="space-y-6 sm:space-y-8">
-          {/* Title */}
-          <div className="space-y-2 md:space-y-4">
-            <h2 className="font-bold text-3xl lg:text-4xl text-gray-800 dark:text-gray-200">
-              We tackle the challenges start-ups face
-            </h2>
-            <p className="text-gray-500">
-              Besides working with start-up enterprises as a partner for
-              digitalization, we have built enterprise products for common pain
-              points that we have encountered in various products and projects.
-            </p>
-          </div>
-          {/* End Title */}
-          {/* List */}
-          <ul role="list" className="space-y-2 sm:space-y-4">
-            <li className="flex space-x-3">
-              <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
-                <svg
-                  className="flex-shrink-0 size-3.5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <polyline points="20 6 9 17 4 12" />
-                </svg>
-              </span>
-              <span className="text-sm sm:text-base text-gray-500">
-                <span className="font-bold">Easy &amp; fast</span> designing
-              </span>
-            </li>
-            <li className="flex space-x-3">
-              <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
-                <svg
-                  className="flex-shrink-0 size-3.5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <polyline points="20 6 9 17 4 12" />
-                </svg>
-              </span>
-              <span className="text-sm sm:text-base text-gray-500">
-                Powerful <span className="font-bold">features</span>
-              </span>
-            </li>
-            <li className="flex space-x-3">
-              <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600 dark:bg-blue-800/30 dark:text-blue-500">
-                <svg
-                  className="flex-shrink-0 size-3.5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <polyline points="20 6 9 17 4 12" />
-                </svg>
-              </span>
-              <span className="text-sm sm:text-base text-gray-500">
-                User Experience Design
-              </span>
-            </li>
-          </ul>
-          {/* End List */}
-        </div>
-      </div>
-      {/* End Col */}
-    </div>
-    {/* End Grid */}
-  </div>
-  {/* End Features */}
-  <div className='grid gap-4 grid-cols-2'>
-    <div>
-        
-        </div>
-
+    return (
         <div>
-            </div> 
-
-  </div>
-</>
-
+            <>
+                {/* Features */}
+                <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+                    {/* Grid */}
+                    <div className="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
+                        <div>
+                            <img
+                                className="rounded-xl"
+                                src="https://media.istockphoto.com/id/1410270664/photo/modern-style-office-with-exposed-concrete-floor-and-a-lot-of-plants.jpg?s=612x612&w=0&k=20&c=lBivR3vIWH4dnb6MUNkQtQsIisaUEnzl2f6Ozyr-Jis="
+                                alt="Image Description"
+                            />
+                        </div>
+                        {/* End Col */}
+                        <div className="mt-5 sm:mt-10 lg:mt-0">
+                            <div className="space-y-6 sm:space-y-8">
+                                <div className="max-w p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+    <div>
+      <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        Brand new
+      </p>
     </div>
-  )
+  </div>
+  <div className="grid max-w-md gap-8 row-gap-10 sm:mx-auto lg:max-w-full lg:grid-cols-3">
+    <div className="flex flex-col sm:flex-row">
+      <div className="sm:mr-4">
+        <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <svg
+            className="w-12 h-12 text-deep-purple-accent-400"
+            stroke="currentColor"
+            viewBox="0 0 52 52"
+          >
+            <polygon
+              strokeWidth={3}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fill="none"
+              points="29 13 14 29 25 29 23 39 38 23 27 23"
+            />
+          </svg>
+        </div>
+      </div>
+      <div>
+        <h6 className="mb-2 font-semibold leading-5">Location</h6>
+        <p className="mb-3 text-sm text-gray-900">
+            .
+        </p>
+        <ul className="mb-4 -ml-1 space-y-2">
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            B-1
+          </li>
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            Lucknow
+          </li>
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            U.P.
+          </li>
+        </ul>
+        <a
+          href="/"
+          aria-label=""
+          className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+        >
+          
+        </a>
+      </div>
+    </div>
+    <div className="flex flex-col sm:flex-row">
+      <div className="sm:mr-4">
+        <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <svg
+            className="w-12 h-12 text-deep-purple-accent-400"
+            stroke="currentColor"
+            viewBox="0 0 52 52"
+          >
+            <polygon
+              strokeWidth={3}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fill="none"
+              points="29 13 14 29 25 29 23 39 38 23 27 23"
+            />
+          </svg>
+        </div>
+      </div>
+      <div>
+        <h6 className="mb-2 font-semibold leading-5">Services</h6>
+        <p className="mb-3 text-sm text-gray-900">
+        .
+        </p>
+        <ul className="mb-4 -ml-1 space-y-2">
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            Transport
+          </li>
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            Canteen
+          </li>
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            Parking
+          </li>
+        </ul>
+        <a
+          href="/"
+          aria-label=""
+          className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+        >
+          
+        </a>
+      </div>
+    </div>
+    <div className="flex flex-col sm:flex-row">
+      <div className="sm:mr-4">
+        <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <svg
+            className="w-12 h-12 text-deep-purple-accent-400"
+            stroke="currentColor"
+            viewBox="0 0 52 52"
+          >
+            <polygon
+              strokeWidth={3}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fill="none"
+              points="29 13 14 29 25 29 23 39 38 23 27 23"
+            />
+          </svg>
+        </div>
+      </div>
+      <div>
+        <h6 className="mb-2 font-semibold leading-5">Price</h6>
+        <p className="mb-3 text-sm text-gray-900">
+          .
+        </p>
+        <ul className="mb-4 -ml-1 space-y-2">
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            Rs1000
+          </li>
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            Rs3000
+          </li>
+          <li className="flex items-start">
+            <span className="mr-1">
+              <svg
+                className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth={4}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </span>
+            Rs5000
+          </li>
+        </ul>
+        <a
+          href="/"
+          aria-label=""
+          className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+        >
+          
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        {/* End Col */}
+                    </div>
+                    {/* End Grid */}
+                </div>
+                {/* End Features */}
+                <div className='max-w p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                    <div className='grid gap-4 grid-cols-2'>
+                        <div className='col-6'>
+                            <h2>Quick Info</h2>
+                            <li>Max Capacity : 4 People</li>
+                            <li>Booking : On Host Confirmation</li>
+                            <li></li>
+                        </div>
+                        <div className='col-6'>
+                            <h2>Open Hours</h2>
+
+                            <li>Mon	08:00 AM - 08:00 PM</li>
+                            <li>Tue	08:00 AM - 08:00 PM</li>
+                            <li> Wed	08:00 AM - 08:00 PM</li>
+                            <li>Thu	08:00 AM - 08:00 PM</li>
+                            <li>Fri	08:00 AM - 08:00 PM</li>
+                            <li>Sat	08:00 AM - 08:00 PM</li>
+                            <li>Sun	Closed</li>
+                        </div>
+
+                    </div>
+                </div>
+            </>
+
+        </div>
+    )
 }
 
 export default spaceDetails;
